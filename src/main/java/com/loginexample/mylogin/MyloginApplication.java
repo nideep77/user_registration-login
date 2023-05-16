0d0f1a072ad375aafb9e47ba02d02f7706641f14
nideep777 <125260911+nideep777@users.noreply.github.com>
2023-05-16 18:53:09 +0530

    first commit

---
+++ src/main/java/com/loginexample/mylogin/MyloginApplication.java
@@ -0,0 +1,14 @@
+package com.loginexample.mylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@SpringBootApplication
+public class MyloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloginApplication.class, args);
+	}
+
+}
